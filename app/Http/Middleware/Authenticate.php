4f108764705c0ae1753cd2c1ad20fd0483213f23
Tuhin Khan <uiu.tuhin@gmail.com>
2022-01-18 00:51:17 +0600

    create auth middleaware

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -14,8 +14,10 @@ class Authenticate extends Middleware
      */
     protected function redirectTo($request)
     {
+        dd($request->user());
         if (! $request->expectsJson()) {
             return route('login');
         }
     }
+
 }
