dd363f1c805d57067feaef383af6f9a508288684
Tuhin Khan <uiu.tuhin@gmail.com>
2022-01-24 00:03:25 +0600

    added installtion step 1

--- resources/views/install/index.blade.php
+++ resources/views/install/index.blade.php
@@ -1,1 +1,22 @@
-hi
+@extends('install.layout')
+@section('content')
+    <form action="/action_page.php">
+        <h1>DTICKET INSTALLATION</h1>
+        <h3 style="text-align: center">Database Configuration</h3>
+        <div class="formcontainer">
+        <hr/>
+        <div class="container">
+        <label for="uname"><strong>Database Host</strong></label>
+        <input type="text" placeholder="Enter Username" name="uname" required>
+        <label for="psw"><strong>Database Name</strong></label>
+        <input type="password" placeholder="Enter Password" name="psw" required>
+
+        <label for="uname"><strong>Database User</strong></label>
+        <input type="text" placeholder="Enter Username" name="uname" required>
+        <label for="psw"><strong>Database Password</strong></label>
+        <input type="password" placeholder="Enter Password" name="psw" required>
+        </div>
+        <button type="submit">Next</button>
+    </form>   
+@endsection
+    
---
+++ resources/views/install/layout.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Simple login form</title>
+    <link href="https90://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet">
+    <link href={{ asset('css/install.css') }} rel="stylesheet">
+    
+  </head>
+  <body>
+    @yield('content')
+  </body>
+</html>
